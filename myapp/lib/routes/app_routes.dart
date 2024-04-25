081ada1929e1114bf830322bc4b16a0450bde300
Juan Camilo Pacheco <jc.pacheco@uniandes.edu.co>
2024-04-24 21:32:15 -0500

    Merge branch 'Pacheco's-Branch' of C:\Users\JuanK\Documents\2024\202410\Moviles\Repo\Flutter with conflicts.

--- myapp/lib/routes/app_routes.dart
+++ myapp/lib/routes/app_routes.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:university_repository/university_repository.dart';
-import '../home/view/home_page/home_page.dart';
-import '../UniversityInfo/view/uiinfo_screen.dart';
-import '../home/view/home_container_screen/home_container_screen.dart';
-import '../home/view/home_tab_container_page/home_tab_container_page.dart';
+import '../screens/home/views/home_container_screen/home_container_screen.dart';
 
 
 class AppRoutes {
